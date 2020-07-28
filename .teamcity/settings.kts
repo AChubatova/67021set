5bc804aa7c3de2924923d3fe247387c3178a1b27
admin <admin@TeamCity>
2020-07-28 05:10:01 -0700

    TeamCity change in '67021' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/settings.kts
@@ -0,0 +1,58 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2020.1"
+
+project {
+
+    vcsRoot(id67021vcs)
+
+    buildType(id67021)
+}
+
+object id67021 : BuildType({
+    id("67021")
+    name = "67021"
+
+    vcs {
+        root(id67021vcs)
+    }
+
+    steps {
+        powerShell {
+            scriptMode = file {
+                path = "projectfile1.ps1"
+            }
+            noProfile = false
+        }
+    }
+})
+
+object id67021vcs : GitVcsRoot({
+    id("67021vcs")
+    name = "67021vcs"
+    url = "https://github.com/AChubatova/67021"
+})
