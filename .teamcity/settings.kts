4676042b3698ecdca7ae7cc3e96d32ef2e4181fd
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-07-28 14:44:54 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -45,7 +45,7 @@ object id67021 : BuildType({
 
     steps {
         powerShell {
-            name = "step1_change"
+            name = "step1_change_setA"
             scriptMode = file {
                 path = "projectfile1.ps1"
             }
